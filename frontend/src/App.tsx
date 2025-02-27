12b27d5cf5d34895b4ed8fa56e137f8224866f86
AnujaLd <isanujadewnim@gmail.com>
2025-02-27 22:47:59 +0530

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import TodoApp from './components/TodoApp'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      <TodoApp></TodoApp>
+      </div>
+    </>
+  )
+}
+
+export default App
